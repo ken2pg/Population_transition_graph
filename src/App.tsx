6c3eeb7c3f891450ef040df783d10cabe2a09ffd
kento <jst34zais9102z@gmail.com>
2021-06-04 20:17:55 +0900

    feat：stateを定義、GETしたデータ（都道府県コード、人口）をstateに格納する関数の作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
-import * as React from "react";
+import * as React from 'react';
+import { getPopulationData, getPrefectureCodes } from './api/requestResas';
+import { CheckboxComponent } from './components/CheckboxComponent';
 
 export const App = () => {
-  return <></>;
+    const [prefectureCodes, setPrefectureCodes] = React.useState([]);
+    const [data, setData] = React.useState([]);
+    const [checkbox, setCheckbox] = React.useState([]);
+
+    const setPopulationData = async (idx: string) => {
+        const a = await getPopulationData(idx + 1);
+        await setData([...data, a]);
+    };
+
+    React.useEffect(() => {
+        setPrefectureCodes([...prefectureCodes, getPrefectureCodes()]);
+    }, []);
+
+    return (
+        <>
+            <CheckboxComponent
+                checkbox={checkbox}
+                setCheckbox={setCheckbox}
+                prefectureCode={prefectureCodes}
+                setPrefectureCode={setPrefectureCodes}
+                data={data}
+                setData={setData}
+            />
+        </>
+    );
 };
