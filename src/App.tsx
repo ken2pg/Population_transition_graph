5dd90638075cfc88211eb22f16496802d0fd95d2
kento <jst34zais9102z@gmail.com>
2021-06-05 23:17:52 +0900

    feat：Graphコンポーネントの追加、displayData　state(表示するグラフ)を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,44 @@
 import * as React from 'react';
 import { getPopulationData, getPrefectureCodes } from './api/requestResas';
 import { CheckboxComponent } from './components/CheckboxComponent';
+import { GraphComponent } from './components/GraphComponent';
+import { prefectureList } from './types/types';
 
 export const App = () => {
-    const [prefectureCodes, setPrefectureCodes] = React.useState([]);
+    const [prefectureCodes, setPrefectureCodes] =
+        React.useState<prefectureList>([]);
     const [data, setData] = React.useState([]);
+    const [displayData, setDisplayData] = React.useState([]);
     const [checkbox, setCheckbox] = React.useState([]);
 
-    const setPopulationData = async (idx: string) => {
+    const setPopulationData = async (idx: number) => {
         const a = await getPopulationData(idx + 1);
         await setData([...data, a]);
     };
 
     React.useEffect(() => {
-        setPrefectureCodes([...prefectureCodes, getPrefectureCodes()]);
-    }, []);
+        getPrefectureCodes(setPrefectureCodes);
+    }, [0]);
 
     return (
         <>
+            <GraphComponent
+                checkbox={checkbox}
+                setCheckbox={setCheckbox}
+                prefectureCode={prefectureCodes}
+                setPrefectureCode={setPrefectureCodes}
+                displayData={displayData}
+                setDisplayData={setDisplayData}
+                data={data}
+                setData={setData}
+            />
             <CheckboxComponent
                 checkbox={checkbox}
                 setCheckbox={setCheckbox}
                 prefectureCode={prefectureCodes}
                 setPrefectureCode={setPrefectureCodes}
+                displayData={displayData}
+                setDisplayData={setDisplayData}
                 data={data}
                 setData={setData}
             />
