9a971806835f6076a0c77597179f9ad90a3a7f54
kento <jst34zais9102z@gmail.com>
2021-06-05 23:29:28 +0900

    feat：cssを追加、チェックボックスをチェックした際の挙動を追加

--- src/components/CheckboxComponent.tsx
+++ src/components/CheckboxComponent.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
+import { getPopulationData } from 'src/api/requestResas';
 import { prefectureList, data } from '../types/types';
+import '../css/CheckboxComponent.css';
 
 interface Props {
     checkbox: number[];
     setCheckbox: (setCheckbox: number[]) => void;
     prefectureCode: prefectureList;
     setPrefectureCode: (setPrefectureList: prefectureList) => void;
+    displayData: data;
+    setDisplayData: (setData: data) => void;
     data: data;
     setData: (setData: data) => void;
 }
@@ -15,8 +19,36 @@ export const CheckboxComponent: React.FC<Props> = ({
     setCheckbox,
     prefectureCode,
     setPrefectureCode,
+    displayData,
+    setDisplayData,
     data,
     setData,
 }) => {
-    return <div></div>;
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (checkbox.includes(Number(e.target.value))) {
+            setCheckbox(
+                checkbox.filter((item) => item !== Number(e.target.value))
+            );
+        } else {
+            setCheckbox([...checkbox, Number(e.target.value)]);
+        }
+    };
+
+    return (
+        <div>
+            {prefectureCode.map((p, i) => {
+                return (
+                    <div key={i}>
+                        <input
+                            type="checkbox"
+                            value={p.prefCode}
+                            onChange={handleChange}
+                            checked={checkbox.includes(p.prefCode)}
+                        />
+                        {p.prefName}
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
