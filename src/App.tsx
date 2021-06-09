7a409d8e9b031d85ae20905f53bc01a34b97546b
kento <jst34zais9102z@gmail.com>
2021-06-09 17:06:31 +0900

    feat：人口データで一度取得したデータは取得しないような関数を作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,73 @@ import * as React from 'react';
 import { getPopulationData, getPrefectureCodes } from './api/requestResas';
 import { CheckboxComponent } from './components/CheckboxComponent';
 import { GraphComponent } from './components/GraphComponent';
-import { prefectureList } from './types/types';
+import { prefectureList, data } from './types/types';
 
 export const App = () => {
     const [prefectureCodes, setPrefectureCodes] =
         React.useState<prefectureList>([]);
-    const [data, setData] = React.useState([]);
+    const [data, setData] = React.useState<data>([]);
     const [displayData, setDisplayData] = React.useState([]);
-    const [checkbox, setCheckbox] = React.useState([]);
+    const [checkbox, setCheckbox] = React.useState<number[]>([]);
 
     const setPopulationData = async (idx: number) => {
-        const a = await getPopulationData(idx + 1);
-        await setData([...data, a]);
+        const a = await getPopulationData(idx, prefectureCodes);
+        await setData([
+            ...data,
+            {
+                label: a.label,
+                data: a.data,
+                fill: true,
+                backgroundColor: a.backgroundColor,
+                borderColor: a.borderColor,
+            },
+        ]);
     };
 
+    const getLabel = (num: number) => {
+        const label = prefectureCodes.map((prefNum) => {
+            if (prefNum.prefCode === num) {
+                return prefNum.prefName;
+            } else {
+                return '';
+            }
+        });
+        //都道府県labelは1つしかないのでlabel[num-1]を返す。
+        //ex. 北海道の場合、["北海道","",""...]のようになっている。
+        return label[num - 1];
+    };
+
+    const setCheckPopulationData = (number: number) => {
+        //都道府県コードに対応する都道府県を取得
+        const label: string = getLabel(number);
+        let isThereLabel = false;
+
+        //もしlabelが存在する場合、fill(表示するかどうか)にtrueをチェック
+        setData((data) =>
+            data.map((item) =>
+                item.label === label ? { ...item, fill: true } : item
+            )
+        );
+
+        //すでにデータを取得済みの場合、isThereLabelをtrueにして、Apiでもう一度取得しない
+        data.map((item) => {
+            if (item.label === label) {
+                isThereLabel = true;
+            }
+        });
+
+        //チェックボックスにチェックが入っており、まだ取得していない場合はApiで取得
+        if (!isThereLabel) {
+            setPopulationData(number);
+        }
+    };
     React.useEffect(() => {
         getPrefectureCodes(setPrefectureCodes);
     }, [0]);
-
     return (
         <>
-            <GraphComponent
+            <h2>人口推移グラフ</h2>
+            <CheckboxComponent
                 checkbox={checkbox}
                 setCheckbox={setCheckbox}
                 prefectureCode={prefectureCodes}
@@ -31,8 +77,10 @@ export const App = () => {
                 setDisplayData={setDisplayData}
                 data={data}
                 setData={setData}
+                setCheckPopulationData={setCheckPopulationData}
+                getLabel={getLabel}
             />
-            <CheckboxComponent
+            <GraphComponent
                 checkbox={checkbox}
                 setCheckbox={setCheckbox}
                 prefectureCode={prefectureCodes}
